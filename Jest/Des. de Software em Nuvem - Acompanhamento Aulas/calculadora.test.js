789b62ff4054fda8e62ae192de34545a240a816a
Guilherme Federico <guilhermeffa@gmail.com>
2021-02-27 16:41:49 -0300

    Atualização

--- Jest/Des. de Software em Nuvem - Acompanhamento Aulas/calculadora.test.js
+++ Jest/Des. de Software em Nuvem - Acompanhamento Aulas/calculadora.test.js
@@ -1,8 +1,17 @@
 const calc = require('./calculadora');
 
-test('Somar 1 e 2 é igual a 3', () => {
-    const soma = calc.somar(1, 2);
-    expect(soma).toBe(3);
+test('Somar 1 e 2 é igual a 3', () => {    
+    //Arrange -> Organizar / Estruturar o que será testado
+    const num1 = 1;
+    const num2 = 2;
+    const expected = 3;
+
+    // Act -> Executar o alvo
+    const soma = calc.somar(num1, num2);
+
+    //Assert -> Verificar se o resultado é o esperado
+    expect(soma).toBe(expected);
+
 });
 
 test('Somar 1.2 e 2.3 é igual a 3.5', () => {
